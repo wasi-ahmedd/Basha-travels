1a13d34e230bd57a0de7437b9f585eccba5ed901
Wasi Ahmed <wasiahemadchoudhary@gmail.com>
2026-03-25 22:38:35 +0530

    fix: prevent status bar from cutting off top content

--- android/app/src/main/java/com/bashatravels/app/MainActivity.java
+++ android/app/src/main/java/com/bashatravels/app/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.parseColor("#0d0d1a"));
         getWindow().setNavigationBarColor(Color.parseColor("#0d0d1a"));
         getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         );
 
         setContentView(R.layout.activity_main);
